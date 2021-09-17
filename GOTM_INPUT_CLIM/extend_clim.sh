40dabc8b5c7f136b265f920db5ccc26112100dd0
Arthur <arthurcapet@gmail.com>
2021-09-17 15:05:38 +0200

    * Updated the notebook for additional variables (Chl, NO2, NO3) * regenerated .nc et .png file * addded extend_clim.sh, which allows to expand the clim files for 5 years to enable spin-up run and comparison to clim

---
+++ GOTM_INPUT_CLIM/extend_clim.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This scripts expands the climatology files to have them valid over the range Jan 2014 -> Dec 2020
+# So it just copies the files, change the years, and concatenate all years in a single file with the same name.
+# A. Capet 17/09/2021
+
+for v in Temp Sal1 PO41 NO3 NO2 Chl O2 Si1 Alk CO2 POC PON POP
+do
+    vo=$v'_clim.txt'
+    for y in 2015 2016 2017 2018 2019 2020
+    do
+	sed s:2014-:$y-: $vo >$vo.$y 
+    done
+    cat $vo* > $vo.ext
+    mv $vo.ext $vo
+    rm $vo.*
+done
+
